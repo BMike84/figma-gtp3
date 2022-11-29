a5560a6f81d669a22474689b6c428854b9dbc906
Michael Buffone <m_buffone@hotmail.com>
2022-11-29 13:02:43 -0500

    added responsive navbar added header section and made responsive and added brand logos section

--- src/components/brand/Brand.js
+++ src/components/brand/Brand.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./brand.css";
+import BrandLogos from "./BrandLogos";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      {BrandLogos.map((val, key) => {
+        return (
+          <div>
+            <img src={val.img} alt="google" />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Brand;
---
+++ src/components/navbar/NavLinks.js
@@ -0,0 +1,24 @@
+const NavLinks = [
+  {
+    link: "#home",
+    title: "Home",
+  },
+  {
+    link: "#wgpt3",
+    title: "What is GPT3?",
+  },
+  {
+    link: "#possibility",
+    title: "Open AI",
+  },
+  {
+    link: "#features",
+    title: "Case Studies",
+  },
+  {
+    link: "#blog",
+    title: "Library",
+  },
+];
+
+export default NavLinks;
