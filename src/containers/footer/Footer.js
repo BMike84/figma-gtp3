4471e0001d6a8d8a7c27d77ff7055eddb55d78ee
Michael Buffone <m_buffone@hotmail.com>
2022-11-30 14:17:38 -0500

    mapped through footer data instead of repeat code

--- src/containers/footer/Footer.js
+++ src/containers/footer/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import FooterData from "./FooterData";
 import gpt3Logo from "../../assets/logo.svg";
 import "./footer.css";
 
@@ -18,25 +19,17 @@ const Footer = () => {
           <img src={gpt3Logo} alt="logo" />
           <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
         </div>
-        <div className="gpt3__footer-links_div">
-          <h4>Links</h4>
-          <p>Overons</p>
-          <p>Social Media</p>
-          <p>Counters</p>
-          <p>Contact</p>
-        </div>
-        <div className="gpt3__footer-links_div">
-          <h4>Company</h4>
-          <p>Terms & Conditions</p>
-          <p>Privacy Policy</p>
-          <p>Contact</p>
-        </div>
-        <div className="gpt3__footer-links_div">
-          <h4>Get in touch</h4>
-          <p>Crechterwoord K12 182 DK Alknjkcb</p>
-          <p>085-132567</p>
-          <p>info@payme.net</p>
-        </div>
+        {FooterData.map((val, index) => {
+          return (
+            <div className="gpt3__footer-links_div" key={index}>
+              <h4>{val.header}</h4>
+              <p>{val.p1}</p>
+              <p>{val.p2}</p>
+              <p>{val.p3}</p>
+              <p>{val.p4}</p>
+            </div>
+          );
+        })}
       </div>
       <div className="gpt3__footer-copyright">
         <p>© 2021 GPT-3. All rights reserved.</p>
---
+++ src/containers/footer/FooterData.js
@@ -0,0 +1,23 @@
+const FooterData = [
+  {
+    header: "Links",
+    p1: "Overons",
+    p2: "Social Medai",
+    p3: "Counters",
+    p4: "Contact",
+  },
+  {
+    header: "Company",
+    p1: "Terms & Conditions",
+    p2: "Privacy Policy",
+    p3: "Contact",
+  },
+  {
+    header: "Get in touch",
+    p1: "Crechterwoord K12 182 DK Alknjkcb",
+    p2: "085-132567",
+    p3: "info@payme.net",
+  },
+];
+
+export default FooterData;
